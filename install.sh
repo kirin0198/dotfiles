cee1db4c221d3ed644de410940c84170e38f562e
kirin0198 <kirin.cero.07@gmail.com>
2019-07-12 11:45:42 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,33 @@
 # git clone https://github.com/kirin0198/dotfiles.git
 
 
+while [[ "$#" -gt 0 ]]; do
+  case "${1}" in
+    '--system' )
+      if [[ -z "${2}" ]] || [[ "${2}" =~ ^--+ ]]; then
+        echo "Unknown option, ${1}. Show usage with --usage option."
+        exit 1
+      else
+        LINUX_SYSTEM="${2}"
+        shift 2
+       fi
+    ;;
+    '--proxy')
+      PROXY_FLAG=1
+      PROXY="${2}"
+      shift 1
+    ;;
+    '--help'|'--usage')
+      usage
+      exit 0
+    ;;
+    *)
+      echo "Unknown option, ${1}. Show usage with --usage option."
+      exit 1
+    ;;
+  esac
+done
+
 ##################################################################33
 # Variables
 ##################################################################33
@@ -60,32 +87,42 @@ done
 # [Deploy]
 # Install for need packageis
 
-if [[ "${1}" == ubuntu ]]; then
-  CMD="apt"
-elif [[ "${1}" == rhel ]]; then
-  CMD="yum"
+if [[ "${LINUX_SYSTEM}" == ubuntu ]]; then
+  CMD="apt install -y"
+elif [[ "${LINUX_SYSTEM}" == rhel ]]; then
+  CMD="yum install -y"
 fi
-${CMD} -y install  epel-release  python36 python36-devel python36-libs python36-pip npm curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker vim
+
+${CMD} epel-release
+${CMD} python36 python36-devel python36-libs python36-pip npm curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker vim
 
 
-#
 # if [[ ${PROXY_FLAG} -eq 1 ]]; then
 #   pip --proxy http://${PROXY}:${PROXY_PORT}
 #   npm -g config set proxy http://${PROXY}:${PORT}
 # fi
-# pip3 install --upgrade pip
-# sudo pip3 install neovim vim-vint pep8 pyflakes yamllint python-language-server
-# npm config set python $(which python)
-# npm config set python $(which python3)
-# npm install --unsafe-perm -g node-inspector
-# sudo npm install -g jsonlint bash-language-server
-#
-# mkdir ~/git && cd $_
-# git clone https://github.com/vim/vim.git
 
-# yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker vim
-# yum install -y ncurses-devel gtk2-devel atk-devel libX11-devel libXt-devel gcc
-# cd ~/git/vim/src
-# ./configure --with-features=huge --enable-gui=gtk2 --enable-pythoninterp --with-python-config-dir=$(which python2.7-config) --enable-python3interp --with-python3-config-dir=$(/usr/bin/python3.6-config) --enable-fail-if-missing
-# sudo make && sudo make install
-# echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> .bashrc or .zshrc
+# pip install
+$(which pip3) install --upgrade pip
+$(which pip3) install neovim vim-vint pep8 pyflakes yamllint python-language-server
+
+
+# npm install
+npm config set python $(which python)
+npm config set python $(which python3)
+npm install --unsafe-perm -g node-inspector
+npm install -g jsonlint bash-language-server
+
+
+mkdir ~/git && cd $_
+git clone https://github.com/vim/vim.git
+
+yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker vim
+yum install -y ncurses-devel gtk2-devel atk-devel libX11-devel libXt-devel gcc
+
+cd ~/git/vim/src
+
+./configure --with-features=huge --enable-gui=gtk2 --enable-pythoninterp --with-python-config-dir=$(which python2.7-config) --enable-python3interp --with-python3-config-dir=$(/usr/bin/python3.6-config) --enable-fail-if-missing
+sudo make && sudo make install
+echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> .bashrc or .zshrc
+echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> .bashrc or .zshrc
